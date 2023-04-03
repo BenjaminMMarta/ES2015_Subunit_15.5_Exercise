4616c5cb63f85a6d037b62d21ac6318eeb157359
Benjamin M. Marta <108314514+BenjaminMMarta@users.noreply.github.com>
2023-04-02 17:08:13 -0700

    Added ES2015 Subunit 15.5 Exercise in JavaScript file.

---
+++ app.js
@@ -0,0 +1,39 @@
+//--------------------Object Enhancement Exercise--------------------//
+
+//Same keys and values//
+function createInstructor(firstName, lastName){
+    return {
+        firstName,
+        lastName,
+    }
+}
+
+//Computed Property Names//
+let favoriteNumber = 42;
+const instructor = {
+  firstName: "Colt",
+  [favoriteNumber]: "That is my favorite!"
+}
+
+//Object Methods//
+const instructor = {
+  firstName: "Colt",
+  sayHi(){
+    return "Hi!";
+  },
+  sayBye(){
+    return this.firstName + " says bye!";
+  }
+}
+
+//createAnimalFunction//
+function createAnimal(species, verb, noise){
+  return {
+    species,
+    [verb](){
+      return noise;
+    }
+  }
+}
+
+//--------------------End--------------------//
